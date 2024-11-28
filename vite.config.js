fc7d59ba4b86503770a6cc086188567d98dca2ef
Alexander Ek <al1510sa-s@student.lu.se>
2024-11-28 17:19:15 +0100

    base on vite.cnfig.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-website/', // Lägg till detta för att använda rätt base path för GitHub Pages
 })
