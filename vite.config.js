28f8ba1c14b26206ef79c56864e1c6c8a2008ca0
Alexander Ek <al1510sa-s@student.lu.se>
2024-11-28 17:26:31 +0100

    erased vite config thing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio-website/', // Lägg till detta för att använda rätt base path för GitHub Pages
+  plugins: [react()]
+   // Lägg till detta för att använda rätt base path för GitHub Pages
 })
