f5c490c9d7515a924908d2a08bf27af33b158bc7
Alexander Ek <al1510sa-s@student.lu.se>
2024-12-04 15:35:02 +0100

    fixing myproject appearence

--- src/containers/my-projects/MyProjects.jsx
+++ src/containers/my-projects/MyProjects.jsx
@@ -6,29 +6,45 @@ const MyProjects = () => {
     <div className='myProjects'>
       <div className='box'>
 
-        <div className='title'>Projects</div>
+        <div className='title-section'>Projects</div>
 
         <div className='project-row'>
           <div className='project-box'>
-            <div className='project-picture'></div>
-            <div className='project-title'>TTime</div>
-            <div className='project-info'>A time reporting system made in Lund university with 18 other student were i was the project leader</div>
+          <div className='project-picture'> <img src="pic_trulli.jpg" alt="Italian Trulli" className='img'></img> </div>
+            <div className='project-title'>Bosch (Thesis)</div>
+            <div className='project-info'>
+            <p style={{ marginBottom: '2rem' }}>
+              (Ongoing) A LLM assisted trend scout for searching after relevant tech news and predict the potential of new technology
+            </p>
+            <p><strong>Languages:</strong> Python</p>
+            <p><strong>Time:</strong> Feb 2025 - Jun 2025</p>
+
+              </div>
   
           </div>
 
           <div className='project-box'>
-          <div className='project-picture'></div>
+          <div className='project-picture'> <img src="pic_trulli.jpg" alt="Italian Trulli" className='img'></img> </div>
+            <div className='project-title'>SAAB</div>
+            <div className='project-info'>A realistic simulation radar of aircrafts for SAAB militairy training</div>
+            
+          </div>
+
+          <div className='project-box'>
+          <div className='project-picture'> <img src="pic_trulli.jpg" alt="Italian Trulli" className='img'></img> </div>
+            <div className='project-title'>Personal Website</div>
+            <div className='project-info'>A personal website of my tech skills and my career</div>
+            
+          </div>
+
+          <div className='project-box'>
+          <div className='project-picture'> <img src="pic_trulli.jpg" alt="Italian Trulli" className='img'></img> </div>
             <div className='project-title'>TTime</div>
             <div className='project-info'>A time reporting system made in Lund university with 18 other student were i was the project leader</div>
             
           </div>
 
-          <div className='project-box'>
-          <div className='project-picture'></div>
-            <div className='project-title'>TTime</div>
-            <div className='project-info'>A time reporting system made in Lund university with 18 other student were i was the project leader</div>
-            
-          </div>
+
 
         </div>
 
